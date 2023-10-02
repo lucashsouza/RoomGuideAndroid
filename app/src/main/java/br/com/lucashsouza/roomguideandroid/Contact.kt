2812e24ba0d6b6ae7287689c0d80c445798c310f
Lucas Souza <lhenriquesouza00@gmail.com>
2023-10-02 19:44:47 -0300

    Initial commit

---
+++ app/src/main/java/br/com/lucashsouza/roomguideandroid/Contact.kt
@@ -0,0 +1,14 @@
+package br.com.lucashsouza.roomguideandroid
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Contact(
+    val firstName: String,
+    val lastName: String,
+    val phoneNumber: String,
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+)
---
+++ app/src/main/java/br/com/lucashsouza/roomguideandroid/SortType.kt
@@ -0,0 +1,7 @@
+package br.com.lucashsouza.roomguideandroid
+
+enum class SortType {
+    FIRST_NAME,
+    LAST_NAME,
+    PHONE_NUMBER
+}
